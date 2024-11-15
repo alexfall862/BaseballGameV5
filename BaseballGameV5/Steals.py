35c1d047cb9158981ad3dfa2a2d919cc0fd02246
Alex <alexfall862@gmail.com>
2024-11-15 15:24:25 -0600

    v

--- BaseballGameV5/Steals.py
+++ BaseballGameV5/Steals.py
@@ -20,33 +20,42 @@ class Steals():
             pass
         else:
             if thirdbase != None:
-                stealrating = Steals.pull_stealfreq(self.thirdbase, self.strategy.playerstrategy)
-                print(f"{stealrating}")
+                #get steal frequency setting from player
+                stealfreqrating = Steals.pull_stealfreq(self.thirdbase, self.strategy.playerstrategy)
+                #roll to see likelihood of attempt
+                diceroll = np.random.rand()
+                if (stealfreqrating > diceroll):
+                    #This should be edited because it's basically a player trying to steal against the catcher throwing it to himself. Might need new formula for just home base evals
+                    output = Steals.calc_baserunning_math(self.gamestate.game.baselines.stealsuccess, self.gamestate.game.baselines.pickoffsuccess, thirdbase, self.gamestate.game.pitchingteam.currentpitcher, self.gamestate.game.pitchingteam.catcher, self.gamestate.game.pitchingteam.thirdbase,)
+                else:
+                    pass
+                #return something
+
             if secondbase != None and thirdbase == None:
-                stealrating = Steals.pull_stealfreq(self.secondbase, self.strategy.playerstrategy)
-                print(f"{stealrating}")
+                stealfreqrating = Steals.pull_stealfreq(self.secondbase, self.strategy.playerstrategy)
+                print(f"{stealfreqrating}")
             if firstbase != None and secondbase == None:
-                stealrating = Steals.pull_stealfreq(self.firstbase, self.strategy.playerstrategy)
-                print(f"{stealrating}")
+                stealfreqrating = Steals.pull_stealfreq(self.firstbase, self.strategy.playerstrategy)
+                print(f"{stealfreqrating}")
                                     
-    def calc_baserunning_math(stealsuccess, pickoffsuccess, baserunner_speed, baserunner_baserunning, baserunner_reaction, catcher_throwpower, catcher_throwacc, catcher_fieldreact, catcher_catch_sequence, pitcher_sequence, pitcher_pickoff, baseman_react, baseman_catch):
-        bsp = (baserunner_speed - 50)/50
-        bbr = (baserunner_baserunning - 50)/50
-        brr = (baserunner_baserunning - 50)/5
+    def calc_baserunning_math(stealsuccess, pickoffsuccess, baserunner, pitcher, catcher, baseman):
+        bsp = (baserunner.speed - 50)/50
+        bbr = (baserunner.baserunning - 50)/50
+        brr = (baserunner.baserunning - 50)/5
         baserunner_scores = [bsp, bbr]
         baserunner_weights = [6, 3]
         baserunner  = ((1+np.average(baserunner_scores, weights=baserunner_weights))*50)+brr
 
-        ctp = (catcher_throwpower - 50)/50
-        cta = (catcher_throwacc - 50)/50
-        ccs = (catcher_catch_sequence - 50)/50
-        pps = (pitcher_sequence - 50)/50
-        cfr = (catcher_fieldreact - 50)/5
+        ctp = (catcher.throwpower - 50)/50
+        cta = (catcher.throwacc - 50)/50
+        ccs = (catcher.catchsequence - 50)/50
+        pps = (pitcher.sequence - 50)/50
+        cfr = (catcher.fieldreact - 50)/5
         battery_scores = [ctp, cta, ccs, pps]
         battery_weights = [15, 3, 1, 1]
         battery = ((1+np.average(battery_scores, weights=battery_weights))*50)+cfr
 
-        pickoffmod = (pitcher_pickoff - 50)/50
+        pickoffmod = (pitcher.pickoff - 50)/50
         pickoffchances = (1+pickoffmod)*pickoffsuccess
 
         comp_score = baserunner/battery
@@ -59,8 +68,11 @@ class Steals():
         else:
             print(f"STEAL FAILURE: {round(steal_outcome_odds, 2)} {round( diceroll, 2)}")
         
-        bmr = (baseman_react - 50)/50
-        bmc = (baseman_catch - 50)/50
+        bmr = (baseman.fieldreact - 50)/50
+        bmc = (baseman.fieldcatch - 50)/50
         
+        #NOT FINISHED, NEED TO FIND WAY TO HANDLE ERRORS AND FIGURE OUT WHAT THE OUTPUT NEEDS TO BE (PROBABLY INCLUDE OUTS OR SOMETHING AS WELL AS BASERUNNER SITUATION)
+
         cta = cta
+        
         
